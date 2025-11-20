e445c58fc4832181659c78aba5d0373850bf4b80
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-20 17:17:31 -0300

    feat(resources): add ClientResource

---
+++ backend/src/main/java/com/aceleradev/backend/resources/ClientResource.java
@@ -0,0 +1,32 @@
+package com.aceleradev.backend.resources;
+
+import com.aceleradev.backend.entities.Client;
+import com.aceleradev.backend.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping
+    public ResponseEntity<List<Client>> findAll(){
+        List<Client> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Client> findById(@PathVariable Long id){
+        Client obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
