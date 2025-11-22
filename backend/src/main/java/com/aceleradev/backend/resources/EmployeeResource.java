5041ca0cf9b5dcfdcb7ecfcbbf4d79347dd3fd5b
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-21 21:59:33 -0300

    feat(resources): add EmployeeResources

---
+++ backend/src/main/java/com/aceleradev/backend/resources/EmployeeResource.java
@@ -0,0 +1,32 @@
+package com.aceleradev.backend.resources;
+
+import com.aceleradev.backend.entities.Employee;
+import com.aceleradev.backend.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/employees")
+public class EmployeeResource {
+
+    @Autowired
+    private EmployeeService service;
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> findAll(){
+        List<Employee> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Employee> findById(@PathVariable Long id){
+        Employee obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
