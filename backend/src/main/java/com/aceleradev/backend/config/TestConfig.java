a04a922465a6aa1ccb67ef97f29cd941566f21d0
nicollaswendel <nicollaswendel1623@gmail.com>
2025-12-23 15:55:40 -0300

    feat: create payment entities

--- backend/src/main/java/com/aceleradev/backend/config/TestConfig.java
+++ backend/src/main/java/com/aceleradev/backend/config/TestConfig.java
@@ -2,6 +2,8 @@ package com.aceleradev.backend.config;
 
 import com.aceleradev.backend.entities.Client;
 import com.aceleradev.backend.entities.Employee;
+import com.aceleradev.backend.entities.Payment;
+import com.aceleradev.backend.entities.enums.Role;
 import com.aceleradev.backend.repositories.ClientRepository;
 import com.aceleradev.backend.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,9 +32,10 @@ public class TestConfig implements CommandLineRunner {
         Client c2 = new Client(null, "Martins Luis", "61982456789", "martinsLuis@email.com", "00000000002", "Garoto Prodigio",LocalDate.now());
         Client c3 = new Client(null, "Dropp", "11987654321", "dropp@email.com", "00000000003", "O Brabo do React", LocalDate.now());
 
-        Employee e1 = new Employee(null, "Random", "11984567121", "random@email.com", "senhaFuncionario", "Dev", 3700.00, "Nosso Latera", LocalDate.now(), null);
+        Employee e1 = new Employee(null, "Random", "11984567121", "random@email.com", "senhaFuncionario", Role.VENDEDOR, 3700.00, "Nosso Latera", LocalDate.now(), null);
 
         clientRepository.saveAll(Arrays.asList(c1, c2, c3));
         employeeRepository.saveAll(List.of(e1)); // List.of() no lugar de asList() pois temos apenas um parâmetro sendo passado
+
     }
 }
