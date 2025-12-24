9971af0d07f00c66a49ef165dcdc28760f2db88b
Luis Martins <luishenrique.martinslima@gmail.com>
2025-12-23 22:00:32 -0300

    feat(User): create abstract User class with @MappedSuperclass
    Co-authored-by: Jerferson Freitas <jeefreitas315@gmail.com>
    Co-authored-by: Herick Danilo <herickdan@hotmail.com>

---
+++ backend/src/main/java/com/aceleradev/backend/entities/Costumer.java
@@ -0,0 +1,12 @@
+package com.aceleradev.backend.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_client")
+public class Costumer extends User {
+
+}
