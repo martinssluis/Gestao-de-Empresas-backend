1fe43ef5aea47984faa99b53c4e0aba36a710fbc
Freitaz <Freitaz@DESKTOP-PNDQIPE>
2026-01-09 23:20:34 -0300

    feat: Create OrderStatus

---
+++ backend/src/main/java/com/aceleradev/backend/entities/enums/OrderStatus.java
@@ -0,0 +1,26 @@
+package com.aceleradev.backend.entities.enums;
+
+public enum OrderStatus {
+    WAINTING_PAYMENT(1),
+    PAID(2),
+    CANCELED(3);
+
+    private int code;
+
+    OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus value : OrderStatus.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid OrderStatus code");
+    }
+
+}
