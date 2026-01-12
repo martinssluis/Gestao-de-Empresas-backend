51e4c449ef4e489a6ba9aaa3691bafe8e5a71c9f
heric <herickdan@hotmail.com>
2026-01-12 20:56:07 -0300

    fix: changing file's place

--- backend/src/main/java/com/aceleradev/backend/repositories/entities/OrderItem.java
+++ backend/src/main/java/com/aceleradev/backend/repositories/entities/OrderItem.java
@@ -1,13 +1,80 @@
 package com.aceleradev.backend.repositories.entities;
 
+import com.aceleradev.backend.repositories.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
-import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
 
-@Table(name="double_primary_key")
-public class OrderItem {
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @EmbeddedId
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private OrderItemPK id = new OrderItemPK();
 
+    private Integer quantity;
+    private Double price;
 
+    public OrderItem(){}
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        super();
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    @JsonIgnore
+    public Order getOrder(){
+        return id.getOrder();
+    }
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getSubtotal(){
+        return price * quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
