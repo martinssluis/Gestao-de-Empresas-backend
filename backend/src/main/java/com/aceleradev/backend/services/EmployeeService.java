325e940f26b4057adfc1e9afe1389126f988ebc9
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-01-22 17:49:07 -0300

    feat: add employee creation using DTO and mapper

--- backend/src/main/java/com/aceleradev/backend/services/EmployeeService.java
+++ backend/src/main/java/com/aceleradev/backend/services/EmployeeService.java
@@ -1,12 +1,14 @@
 package com.aceleradev.backend.services;
 
+import com.aceleradev.backend.commons.dto.employee.CreateEmployeeDTO;
 import com.aceleradev.backend.entities.Employee;
+import com.aceleradev.backend.mapper.EmployeeMapper;
 import com.aceleradev.backend.repositories.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -18,14 +20,21 @@ public class EmployeeService {
         return repository.findAll();
     }
 
-    public Employee findById(Long id){
-        Optional<Employee> obj = repository.findById(id);
-        return obj.get();
+    public Employee findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Employee not found."));
+    }
+
+    public Employee create(CreateEmployeeDTO dto) {
+        Employee employee = EmployeeMapper.toEntity(dto);
+        return repository.save(employee);
+    }
 
         // padronizar igual a ClientService? se o ID não existir = 500 Internal Server Error
         // public Employee findById(Long id){
         //    return repository.findById(id)
         //        .orElseThrow(() -> new EntityNotFoundException("Employee not found"));
         //}
-    }
+    // feito
+
 }
