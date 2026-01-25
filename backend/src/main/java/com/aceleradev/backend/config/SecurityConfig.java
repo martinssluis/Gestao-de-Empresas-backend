30e6ee1fb9e8c538a082f071275ca33c47f45cce
heric <herickdan@hotmail.com>
2026-01-24 21:41:45 -0300

    security-config

---
+++ backend/src/main/java/com/aceleradev/backend/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.aceleradev.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
