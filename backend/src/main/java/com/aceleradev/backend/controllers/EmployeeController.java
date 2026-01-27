608467efd2df5c62309dceab997f04682bd62e29
heric <herickdan@hotmail.com>
2026-01-26 22:14:43 -0300

    employee crud completed

--- backend/src/main/java/com/aceleradev/backend/controllers/EmployeeController.java
+++ backend/src/main/java/com/aceleradev/backend/controllers/EmployeeController.java
@@ -1,32 +1,54 @@
 package com.aceleradev.backend.controllers;
 
+import com.aceleradev.backend.commons.dto.EmployeeDto;
 import com.aceleradev.backend.entities.Employee;
 import com.aceleradev.backend.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/employee")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService service;
 
     @GetMapping
-    public ResponseEntity<List<Employee>> findAll(){
-        List<Employee> list = service.findAll();
+    public ResponseEntity<List<EmployeeDto>> findAll() {
+        List<EmployeeDto> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Employee> findById(@PathVariable Long id){
-        Employee obj = service.findById(id);
+    public ResponseEntity<EmployeeDto> findById(@PathVariable Long id) {
+        EmployeeDto obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createEmployee(
+            @RequestBody EmployeeDto dto
+    ) {
+        service.createEmployee(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(
+            @PathVariable Long id
+    ) {
+        service.deleteEmployee(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateEmployee(
+            @PathVariable Long id,
+            @RequestBody EmployeeDto dto
+    ) {
+        service.updateEmployee(id, dto);
     }
 }
